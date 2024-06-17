bebf107990d96174a7d081ddf3d2a2a7fdab169a
Ehren Strifling <ehrenstrifling@gmail.com>
2024-06-17 13:52:33 -0500

    Add db context and starting objects

---
+++ ShopNexus.Entities/Context/ShopNexusContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopNexus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Context
+{
+    public class ShopNexusContext : DbContext
+    {
+        public ShopNexusContext() : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=ShopNexusDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ ShopNexus.Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ShopNexus.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+    }
+}
