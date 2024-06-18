475b9e054f233ca527b7128af22dbc0ff4afee06
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2024-06-18 15:24:55 -0500

    Header completed

---
+++ shopnexus.client/src/components/Header.js
@@ -0,0 +1,46 @@
+import {useState} from 'react'
+import { SlMagnifier } from "react-icons/sl";
+import { FaRegUser } from "react-icons/fa";
+import { IoCartSharp } from "react-icons/io5";
+import '../css/header.css'
+
+function Header() {
+    const [isClicked, setIsClicked] = useState(false)
+
+    return (
+        <header>
+            <h1>Shop<span>Nexus</span></h1> 
+            <nav>
+                <ul>
+                    <li>Categories</li>
+                    <li>Offers</li>
+                    <li>About Us</li>
+                </ul>
+            </nav>  
+            <div className='search-product'>
+                <div className='input-wrapper'>
+                    <input
+                        type='text'
+                        placeholder={`Search your products here `}
+                    />
+                    <SlMagnifier className='magnifier-icon'/>
+                </div>
+            </div>
+            <div className='access-btns'>
+                <span onClick={() => {setIsClicked(isClicked ? false : true)}}><FaRegUser/> Account</span>
+                <div className='btns-modal' style={{display: isClicked ? "block" : "none"}}>
+                    <div className='btns-wrapper'>
+                        <span></span>
+                        <button className='login-btn btn'>Log In</button> 
+                        <button className='signup-btn btn'>Sign Up</button>
+                    </div>
+                </div>
+            </div> 
+            <div className='cart-section'>
+                <span><IoCartSharp/> Cart</span>
+            </div>    
+        </header>
+    )
+}
+
+export default Header
