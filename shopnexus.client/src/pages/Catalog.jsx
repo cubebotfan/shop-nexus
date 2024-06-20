8563ee4310fc5c010af4577adbe3bde60d5bc348
Ehren Strifling <ehrenstrifling@gmail.com>
2024-06-20 12:03:23 -0500

    merge AllProducts and cataloge

--- shopnexus.client/src/pages/Catalog.jsx
+++ shopnexus.client/src/pages/Catalog.jsx
@@ -1,25 +1,36 @@
 import ProductCard from '../components/ProductCard'
 import '../css/catalog.css'
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
-function Catalog({ products }) {
-    const [productsList, setProducts] = useState(null)
+function Catalog() {
+    const [products, setProducts] = useState([]);
 
-    useEffect(() => { setProducts(products) }, [])
-    useEffect(() => { console.log("This are my products", productsList) }, [productsList])
+    useEffect(() => {
+        const apiLink = 'https://localhost:7182/api';
+        axios({
+            url: `${apiLink}/products`,
+            method: "GET"
+        }).then(res => {
+            setProducts(res.data);
+        }).catch(error => {
+            console.error('Cannot get products from api');
+        })
+    }, []);
 
     return (
-        <div className='container'>
+        <main className='container'>
             <div className="catalog-content">
                 <h2>All the products</h2>
-                <div className="catalog">
-                    {productsList && productsList.map(product => (
-                        <ProductCard name={product.name} price={product.price} imgSrc={product.imageURL} />
-                    ))}
-
+                <div className='catalog'>
+                    {products.map(product => {
+                        return (
+                            <ProductCard key={product.productId} name={product.name} productId={product.productId} price={product.price.toFixed(2)} imgSrc={product.imageURL} />
+                        )
+                    })}
                 </div>
             </div>
-        </div>
+        </main>
     )
 }
 
