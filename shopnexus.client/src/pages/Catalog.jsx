6fbc84d2b5028f442ace069f226621496aa47c99
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2024-06-20 10:57:17 -0500

    Catalog updated

--- shopnexus.client/src/pages/Catalog.jsx
+++ shopnexus.client/src/pages/Catalog.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
 import ProductCard from './ProductCard'
 import '../css/catalog.css'
+import { useState, useEffect } from "react";
 
-function Catalog() {
-  return (
-    <div className='container'>
-        <div className="catalog-content">
-            <h2>All the products</h2>
-            <div className="catalog">
-                <ProductCard name="Pulp Fiction T-Shirt" price="$29.99" imgSrc="../img/arun-clarke-ZqnlW6EAel0-unsplash.jpg" />
-                <ProductCard name="Social Savage" price="$39.99" imgSrc="../img/the-graphic-space-FTrGeAy0RW4-unsplash.jpg" />
-                <ProductCard name="Wild Bright" price="$49.99" imgSrc="../img/toa-heftiba-9PVUNBgqVRo-unsplash.jpg" />
-                <ProductCard name="Pulp Fiction T-Shirt" price="$29.99" imgSrc="../img/arun-clarke-ZqnlW6EAel0-unsplash.jpg" />
-                <ProductCard name="Social Savage" price="$39.99" imgSrc="../img/the-graphic-space-FTrGeAy0RW4-unsplash.jpg" />
-                <ProductCard name="Wild Bright" price="$49.99" imgSrc="../img/toa-heftiba-9PVUNBgqVRo-unsplash.jpg" />
-                <ProductCard name="Pulp Fiction T-Shirt" price="$29.99" imgSrc="../img/arun-clarke-ZqnlW6EAel0-unsplash.jpg" />
-                <ProductCard name="Social Savage" price="$39.99" imgSrc="../img/the-graphic-space-FTrGeAy0RW4-unsplash.jpg" />
-                <ProductCard name="Wild Bright" price="$49.99" imgSrc="../img/toa-heftiba-9PVUNBgqVRo-unsplash.jpg" />
-                <ProductCard name="Pulp Fiction T-Shirt" price="$29.99" imgSrc="../img/arun-clarke-ZqnlW6EAel0-unsplash.jpg" />
-                <ProductCard name="Social Savage" price="$39.99" imgSrc="../img/the-graphic-space-FTrGeAy0RW4-unsplash.jpg" />
-                <ProductCard name="Wild Bright" price="$49.99" imgSrc="../img/toa-heftiba-9PVUNBgqVRo-unsplash.jpg" />
+function Catalog({ products }) {
+    const [productsList, setProducts] = useState(null)
+
+    useEffect(() => { setProducts(products) }, [])
+    useEffect(() => { console.log("This are my products", productsList) }, [productsList])
+
+    return (
+        <div className='container'>
+            <div className="catalog-content">
+                <h2>All the products</h2>
+                <div className="catalog">
+                    {productsList && productsList.map(product => (
+                        <ProductCard name={product.name} price={product.price} imgSrc={product.imageURL} />
+                    ))}
+
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Catalog
